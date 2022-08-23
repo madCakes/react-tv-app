b8d8162eb6ade105b611efff0fa63e3281123c23
madCakes <jason.pbrooks@hotmail.com>
2022-08-23 12:02:33 +0100

    refactor components

--- src/components/ShowCard/index.jsx
+++ src/components/ShowCard/index.jsx
@@ -3,7 +3,9 @@ import React from "react"
 const ShowCard = ({data}) => {
 
     return <div className="show-card">
+                <img src={data["image"] ? data["image"]["medium"] : ""}></img>
                 <h2>{data.name}</h2>
+                <p>{data.summary}</p>
            </div>
 
 }
