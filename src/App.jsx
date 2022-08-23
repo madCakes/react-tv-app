05599e097d2b189ddfd7052e14d8821ede39ce10
madCakes <jason.pbrooks@hotmail.com>
2022-08-23 10:58:59 +0100

    Here come the girls

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+
+import ShowCard from './components/ShowCard'
+
+import './App.css'
 
 const App = () => {
+
+    const [showData, setShowData] = useState([])
+
+    useEffect(()=> {
+        async function searchAPI(){
+            const result = await axios.get("https://api.tvmaze.com/search/shows?q=girls");
+            setShowData(result.data)
+        }
+
+        searchAPI()
+    }, [])
+
     
-    return <h1>Hello World!</h1>
+    return <>
+    
+        {showData.map (s => <ShowCard key={s["show"]["id"]} data={s["show"]}/>)}
+        </>
+    
 }
 
 export default App;
