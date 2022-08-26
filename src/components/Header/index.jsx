4c379f5cc4e845e9efafec74a22d51b6c728f2d1
madCakes <jason.pbrooks@hotmail.com>
2022-08-26 10:32:53 +0100

    add mocks for tests

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import "./styles.css";
 
 const Header = () => {
-    return  <header>
-                <h1>Matt's TV Extravaganza</h1>
-                <em>Copywrite gleefully violated!</em>
-            </header>
-}
+  return (
+    <header>
+      <h1>Matt's TV Extravaganza</h1>
+      <em>Copywrite gleefully violated!</em>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
